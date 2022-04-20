78cc3c59260b33823dd59f6e299ffac819555310
alenic <alenic@pop-os.localdomain>
2022-04-20 22:58:47 +0200

    added imagefolder to init

--- pytorchtools/datasets/__init__.py
+++ pytorchtools/datasets/__init__.py
@@ -1,8 +1,10 @@
 from .inference_dataset import InferenceDataset
 from .robustness_dataset import RobustnessDataset, robustness_config, valid_transforms
+from .imagefolder import ImageFolderDataset
 
 
 __all__ = ["InferenceDataset",
            "RobustnessDataset",
            "robustness_config",
-           "valid_transforms"]
+           "valid_transforms",
+           "ImageFolderDataset"]
